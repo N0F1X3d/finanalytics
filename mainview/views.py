bcf5454d2cba49fb921d55ce12d8a565541e13e0
N0F1X3d <danilshpilka@yandex.ru>
2024-10-06 12:00:07 +0300

    Finding securitie + first prototype chart

--- mainview/views.py
+++ mainview/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from moex_iss_api import get_all_securities
+from django.http import JsonResponse
+from moex_iss_api import get_all_securities, take_data_frame
 
 def homepageview(request):
     return render(request, 'mainview/mainview.html')
@@ -7,3 +8,15 @@ def homepageview(request):
 def securities_page(request):
     securities_list = get_all_securities.get_securities_list()
     return render(request, 'mainview/securities.html', {'securities': securities_list})
+
+def securitie_price_chart(request, ticker):
+    script, div = take_data_frame.get_script_div(ticker)
+    return render(request, 'moex_iss_api/securitie_price.html', {'script':script, 'div':div, 'ticker':ticker})
+
+def search_securities(request):
+    query = request.GET.get('query', '')
+    if query:
+        securities_list = get_all_securities.get_securities_list()
+        filtered_securities = [sec for sec in securities_list if query.lower() in sec['SHORTNAME'].lower()]
+        return JsonResponse(filtered_securities, safe=False)
+    return JsonResponse([], safe=False)
