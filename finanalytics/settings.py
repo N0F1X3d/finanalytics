41a493e75163c4d5d66897418412a170bbae10bc
N0F1X3d <danilshpilka@yandex.ru>
2024-12-18 19:34:39 +0300

    deploy

--- finanalytics/settings.py
+++ finanalytics/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['finanalytics.onrender.com']
 
 
 # Application definition
